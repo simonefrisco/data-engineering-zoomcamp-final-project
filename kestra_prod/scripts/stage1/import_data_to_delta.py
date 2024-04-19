ac89d400e60fb895a9364f2f99f7a9e97bf39506
simonefrisco <simonefrisco@gmail.com>
2024-04-19 16:32:00 +0200

    new version

--- kestra_prod/scripts/stage1/import_data_to_delta.py
+++ kestra_prod/scripts/stage1/import_data_to_delta.py
@@ -13,29 +13,42 @@ uri_campaigns = "{{ vars.aws_s3_bucket }}/deltalake/l0_campaigns"
 uri_client_first_purchase_date = "{{ vars.aws_s3_bucket }}/deltalake/l0_client_first_purchase_date"
 uri_holidays = "{{ vars.aws_s3_bucket }}/deltalake/l0_holidays"
 
-print('1. Dump CSV Campaigns Table')
+print('1. Upload Campaigns Table')
 (
     pl
     .read_csv('{{workingDir}}/campaigns.csv', try_parse_dates=True)
-    .sink_parquet("{{workingDir}}/campaigns.parquet")
+    .write_delta(
+            uri_campaigns,
+            mode='append',
+            storage_options=storage_options,
+    )
 )
 
-print('2. Dump CSV Client First Date')
+print('2. Upload Client First Date')
 (
     pl
     .read_csv('{{workingDir}}/client_first_purchase_date.csv', try_parse_dates=True)
-    .sink_parquet("{{workingDir}}/client_first_purchase_date.parquet")
+    .write_delta(
+            uri_client_first_purchase_date,
+            mode='append',
+            storage_options=storage_options,
+    )
 )
 
-print('3. Dump CSV Holidays')
+print('3. Upload Holidays')
 (
     pl
     .read_csv('{{workingDir}}/holidays.csv', try_parse_dates=True)
-    .sink_parquet("{{workingDir}}/holidays.parquet")
-
+    .write_delta(
+            uri_holidays,
+            mode='append',
+            storage_options=storage_options,
+    )
 )
 
-print('4.  Dump CSV Messages')
+print('4.  Upload Messages')
+print('4.1 Convert csv in parquet')
+
 (
     pl
     .scan_csv("{{workingDir}}/messages-demo.csv",try_parse_dates=True)
@@ -44,3 +57,18 @@ print('4.  Dump CSV Messages')
     ])
     .sink_parquet("{{workingDir}}/messages-demo.parquet")
 )
+
+import time
+time.sleep(2)
+
+print('4.2 Upload parquet in chunks')
+parquet_file = pq.ParquetFile("{{workingDir}}/messages-demo.parquet")
+
+for i, chunk in enumerate(parquet_file.iter_batches(200_000)):
+    print(f'loading {i} chuck-th .. ')
+    pl.from_arrow(chunk).write_delta(
+            uri_messages,
+            mode = 'append',
+            delta_write_options={'partition_by' : 'date_day' },
+            storage_options=storage_options,
+    )
